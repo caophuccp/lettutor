578f1e60579ff2a117db1536b0f2c479b40da94c
caophuccp <cna0x11@gmail.com>
2021-10-24 16:34:10 +0700

    Add total lesson time to UpcomingScreen

--- lib/screens/upcoming_screen/upcoming_screen.dart
+++ lib/screens/upcoming_screen/upcoming_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:lettutor/components/lesson_date_session.dart';
 import 'package:lettutor/extensions/navigate_extensions.dart';
 import 'package:lettutor/screens/upcoming_screen/meeting_screen.dart';
+import 'package:lettutor/styles/consts.dart';
+import 'package:lettutor/styles/text_styles.dart';
 
 class UpcomingScreen extends StatefulWidget {
   const UpcomingScreen({Key? key}) : super(key: key);
@@ -16,37 +18,55 @@ class _UpcomingScreenState extends State<UpcomingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              LessonDateSession(
-                isExpanded: true,
-                date: 'Sat 23 Oct 21',
-                showTutorReview: false,
-                isCancellable: true,
-                countdown: true,
-                onCancel: cancel,
-                joinMeeting: joinMeeting,
-                directMessage: directMessage,
+        child: Column(
+          children: [
+            Container(
+                width: double.infinity,
+                height: 40,
+                alignment: Alignment.center,
+                child: Text(
+                  'Total lesson time is 12 hours 55 minutes',
+                  style: TextStyles.subtitle1SemiBold.copyWith(
+                    color: Colors.white
+                  ),
+                ),
+                color: Colors.blue,
               ),
-              LessonDateSession(
-                date: 'Thu 21 Oct 21',
-                showTutorReview: false,
-                isCancellable: true,
-                onCancel: cancel,
-                joinMeeting: joinMeeting,
-                directMessage: directMessage,
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    LessonDateSession(
+                      isExpanded: true,
+                      date: 'Sat 23 Oct 21',
+                      showTutorReview: false,
+                      isCancellable: true,
+                      countdown: true,
+                      onCancel: cancel,
+                      joinMeeting: joinMeeting,
+                      directMessage: directMessage,
+                    ),
+                    LessonDateSession(
+                      date: 'Thu 21 Oct 21',
+                      showTutorReview: false,
+                      isCancellable: true,
+                      onCancel: cancel,
+                      joinMeeting: joinMeeting,
+                      directMessage: directMessage,
+                    ),
+                    LessonDateSession(
+                      date: 'Wed 20 Oct 21',
+                      showTutorReview: false,
+                      isCancellable: true,
+                      onCancel: cancel,
+                      joinMeeting: joinMeeting,
+                      directMessage: directMessage,
+                    ),
+                  ],
+                ),
               ),
-              LessonDateSession(
-                date: 'Wed 20 Oct 21',
-                showTutorReview: false,
-                isCancellable: true,
-                onCancel: cancel,
-                joinMeeting: joinMeeting,
-                directMessage: directMessage,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
