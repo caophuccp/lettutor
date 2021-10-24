39b23b1fb8336cf08f2b2dda70b63691849d5ac8
caophuccp <cna0x11@gmail.com>
2021-10-24 16:44:42 +0700

    Use system theme

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
           selectedItemColor: Colors.white,
         ),
       ),
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.system,
       home: MainScreen(),
     );
   }
